e21ed318995a59e5c2c9d4c05fdba821014a02b7
Elad Moshe <eladm26@gmail.com>
2025-04-18 13:15:22 +0300

    commit 6

---
+++ api/services/sales/main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"expvar"
+	"fmt"
+	"os"
+	"os/signal"
+	"runtime"
+	"syscall"
+	"time"
+
+	"github.com/ardanlabs/conf/v3"
+	"github.com/ardanlabs/service/foundation/logger"
+)
+
+var build = "develop"
+
+func main() {
+	var log *logger.Logger
+
+	events := logger.Events{
+		Error: func(ctx context.Context, r logger.Record) {
+			log.Info(ctx, "******* SEND ALERT *******")
+		},
+	}
+
+	traceIDFn := func(ctx context.Context) string {
+		return "" //web.GetTraceID(ctx)
+	}
+
+	log = logger.NewWithEvents(os.Stdout, logger.LevelInfo, "SALES", traceIDFn, events)
+
+	// ------------------------------------------------------------------------------------- //
+
+	ctx := context.Background()
+
+	if err := run(ctx, log); err != nil {
+		log.Error(ctx, "startup", "msg", err)
+		os.Exit(1)
+	}
+
+}
+
+func run(ctx context.Context, log *logger.Logger) error {
+	log.Info(ctx, "startup", "GOMAXPROCS", runtime.GOMAXPROCS(0))
+
+	// -------------------------------------------------------------------------
+	// Configuration
+
+	cfg := struct {
+		conf.Version
+		Web struct {
+			ReadTimeout        time.Duration `conf:"default:5s"`
+			WriteTimeout       time.Duration `conf:"default:10s"`
+			IdleTimeout        time.Duration `conf:"default:120s"`
+			ShutdownTimeout    time.Duration `conf:"default:20s"`
+			APIHost            string        `conf:"default:0.0.0.0:3000"`
+			DebugHost          string        `conf:"default:0.0.0.0:3010"`
+			CORSAllowedOrigins []string      `conf:"default:*,mask"`
+		}
+	}{
+		Version: conf.Version{
+			Build: build,
+			Desc:  "Sales",
+		},
+	}
+
+	const prefix = "SALES"
+	help, err := conf.Parse(prefix, &cfg)
+	if err != nil {
+		if errors.Is(err, conf.ErrHelpWanted) {
+			fmt.Println(help)
+			return nil
+		}
+		return fmt.Errorf("parsing config: %w", err)
+	}
+
+	// -------------------------------------------------------------------------
+	// App Starting
+
+	log.Info(ctx, "starting service", "version", cfg.Build)
+	defer log.Info(ctx, "shutdown complete")
+
+	out, err := conf.String(&cfg)
+	if err != nil {
+		return fmt.Errorf("generating config for output: %w", err)
+	}
+	log.Info(ctx, "startup", "config", out)
+
+	expvar.NewString("build").Set(cfg.Build)
+
+	// -------------------------------------------------------------------------
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+	sig := <-shutdown
+
+	log.Info(ctx, "shutdown", "status", "shutdown started", "signal", sig)
+	defer log.Info(ctx, "shutdown", "status", "shutdown complete", "signal", sig)
+
+	return nil
+}
--- go.mod
+++ go.mod
@@ -4,4 +4,9 @@ go 1.24
 
 toolchain go1.24.1
 
-require github.com/go-json-experiment/json v0.0.0-20250223041408-d3c622f1b874
+require (
+	github.com/ardanlabs/conf/v3 v3.7.1
+	github.com/go-json-experiment/json v0.0.0-20250223041408-d3c622f1b874
+)
+
+require golang.org/x/text v0.23.0 // indirect

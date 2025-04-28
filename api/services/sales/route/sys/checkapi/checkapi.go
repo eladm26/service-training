7831c4ef777b6031cea832ce7d6eeb82ad93b976
Elad Moshe <eladm26@gmail.com>
2025-04-28 23:16:01 +0300

    commit 12

--- api/services/sales/route/sys/checkapi/checkapi.go
+++ api/services/sales/route/sys/checkapi/checkapi.go
@@ -5,24 +5,26 @@ import (
 	"context"
 	"encoding/json"
 	"net/http"
+
+	"github.com/ardanlabs/service/foundation/web"
 )
 
-func liveness(ctx context.Context,w  http.ResponseWriter, r *http.Request) error {
+func liveness(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
 	status := struct {
 		Status string
 	}{
 		Status: "OK",
 	}
 
-	return json.NewEncoder(w).Encode(status)
+	return web.Respond(ctx, w, status, http.StatusOK)
 }
 
-func readiness(ctx context.Context, w http.ResponseWriter, r *http.Request) error{
+func readiness(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
 	status := struct {
 		Status string
 	}{
 		Status: "OK",
 	}
 
-	return json.NewEncoder(w).Encode(status)
+	return web.Respond(ctx, w, status, http.StatusOK)
 }

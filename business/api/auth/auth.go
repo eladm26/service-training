7765148c12f6e77579cf070bcac670fe8962ee91
Elad Moshe <eladm26@gmail.com>
2025-05-08 13:01:12 +0300

    commit 26

--- business/api/auth/auth.go
+++ business/api/auth/auth.go
@@ -39,7 +39,7 @@ func (c Claims) HasRole(r string) bool {
 // PEM encoded string or a JWS based key.
 type KeyLookup interface {
 	PrivateKey(kid string) (key string, err error)
-	PubliceKey(kid string) (key string, err error)
+	PublicKey(kid string) (key string, err error)
 }
 
 // COnfig trpresents information required to initialize auth
@@ -115,7 +115,7 @@ func (a *Auth) Authenticate(ctx context.Context, bearerToken string) (Claims, er
 		return Claims{}, fmt.Errorf("kid malformed: %w", err)
 	}
 
-	pem, err := a.keyLookup.PubliceKey(kid)
+	pem, err := a.keyLookup.PublicKey(kid)
 	if err != nil {
 		return Claims{}, fmt.Errorf("failed to fetch public key: %w", err)
 	}
@@ -142,17 +142,17 @@ func (a *Auth) Authenticate(ctx context.Context, bearerToken string) (Claims, er
 // none of the input roles are within the user's claims, we return an error
 // otherwise the user is authorized.
 func (a *Auth) Authorize(ctx context.Context, claims Claims, userID uuid.UUID, rule string) error {
-    input := map[string]any{
-        "Roles": claims.Roles,
-        "Subject": claims.Subject,
-        "UserID": userID,
-    }
+	input := map[string]any{
+		"Roles":   claims.Roles,
+		"Subject": claims.Subject,
+		"UserID":  userID,
+	}
 
-    if err := a.opaPolicyEvaluation(ctx, regoAuthorization, rule, input); err != nil {
-        return fmt.Errorf("rego evaluation failed: %w", err)
-    }
+	if err := a.opaPolicyEvaluation(ctx, regoAuthorization, rule, input); err != nil {
+		return fmt.Errorf("rego evaluation failed: %w", err)
+	}
 
-    return nil
+	return nil
 }
 
 // opaPolicyEvaluation asks opa to evaluate the token against the specified token
@@ -188,23 +188,23 @@ func (a *Auth) opaPolicyEvaluation(ctx context.Context, regoScript string, rule 
 // isUserEnabled hits the database and checks the user is not disabled. If the
 // no database connection was provided, this check is skipped.
 // func (a *Auth) isUserEnabled(ctx context.Context, claims Claims) error {
-	// if a.userBus == nil {
-	// 	return nil
-	// }
+// if a.userBus == nil {
+// 	return nil
+// }
 
-	// userID, err := uuid.Parse(claims.Subject)
-	// if err != nil {
-	// 	return fmt.Errorf("parse user: %w", err)
-	// }
+// userID, err := uuid.Parse(claims.Subject)
+// if err != nil {
+// 	return fmt.Errorf("parse user: %w", err)
+// }
 
-	// usr, err := a.userBus.QueryByID(ctx, userID)
-	// if err != nil {
-	// 	return fmt.Errorf("query user: %w", err)
-	// }
+// usr, err := a.userBus.QueryByID(ctx, userID)
+// if err != nil {
+// 	return fmt.Errorf("query user: %w", err)
+// }
 
-	// if !usr.Enabled {
-	// 	return fmt.Errorf("user disabled")
-	// }
+// if !usr.Enabled {
+// 	return fmt.Errorf("user disabled")
+// }
 
-	// return nil
+// return nil
 // }
